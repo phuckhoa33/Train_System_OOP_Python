d18b87b11450f28403c5dd26d2846cc827b2e70c
33phuckhoa <mphuc8671@gmail.com>
2023-04-29 05:03:28 +1400

    Add all feature include User, Chair object improvement and package all objects

---
+++ Contact/PhoneNumber.py
@@ -0,0 +1,20 @@
+import re 
+
+class PhoneNumber():
+    def __init__(self, phone_number: int) -> None:
+        if self.is_valid_vietnamese_phone_number(phone_number):
+            self.__phone_number = phone_number
+        else: 
+            raise ValueError("Invalid vietnamese phone number")
+        
+    def is_valid_vietnamese_phone_number(self):
+        """
+        Check if the phone number is in the correct format for Vietnamese phone numbers
+        """
+        # Regular expression for the format of Vietnamese phone numbers
+        regex = r"^\+84\d{9}$|^0\d{9}$|^84\d{9}$"
+
+        if not re.match(regex, self.telephoneNumber):
+            return False
+        else:
+            return True
---
+++ Enum/GenderEnum.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class GenderState(Enum):
+    MALE: int = 1
+    FEMAIL: int = 0
