c79c65178c0a4cef2496c06ed1aee2a2a7ebb795
phuckhoa33 <phuckhoa81@gmail.com>
2023-05-08 18:01:54 +0700

    Replace database file and invalid record

--- database/instantiate_database.sql
+++ database/instantiate_database.sql
@@ -2,6 +2,14 @@ CREATE DATABASE train_system;
 
 USE train_system;
 
+CREATE TABLE address(
+	address_id INT NOT NULL AUTO_INCREMENT,
+    communce VARCHAR(30),
+    distict VARCHAR(30),
+    city VARCHAR(30),
+    country VARCHAR(30),
+    PRIMARY KEY(address_id)
+);
 CREATE TABLE user(
 	user_id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(30),
@@ -13,14 +21,6 @@ CREATE TABLE user(
     FOREIGN KEY(address_id) REFERENCES address(address_id)
 );
 
-CREATE TABLE address(
-	address_id INT NOT NULL AUTO_INCREMENT,
-    communce VARCHAR(30),
-    distict VARCHAR(30),
-    city VARCHAR(30),
-    country VARCHAR(30),
-    PRIMARY KEY(address_id)
-);
 
 CREATE TABLE payment (
 	payment_id INT NOT NULL AUTO_INCREMENT,
@@ -30,17 +30,22 @@ CREATE TABLE payment (
     PRIMARY KEY(payment_id),
     FOREIGN KEY(user_id) REFERENCES user(user_id)
 );
-
-CREATE TABLE chair (
-	chair_id INT NOT NULL AUTO_INCREMENT,
-    wagon_id INT, 
-    user_id INT,
-    state VARCHAR(10),
-    PRIMARY KEY(chair_id),
-    chair_type VARCHAR(10),
-    FOREIGN KEY(wagon_id) REFERENCES wagon(wagon_id)
+CREATE TABLE staff (
+	staff_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50),
+    fullname VARCHAR(50),
+    telephoneNumber VARCHAR(10),
+    email VARCHAR(50),
+    gender INT,
+    address_id INT,
+    PRIMARY KEY(staff_id)
 );
 
+CREATE TABLE train(
+	train_id INT NOT NULL AUTO_INCREMENT,
+    train_name VARCHAR(10),
+    PRIMARY KEY(train_id)
+);
 CREATE TABLE wagon(
 	wagon_id INT NOT NULL AUTO_INCREMENT,
     train_id INT,
@@ -51,24 +56,14 @@ CREATE TABLE wagon(
 	PRIMARY KEY(wagon_id),
     FOREIGN KEY(train_id) REFERENCES train(train_id)
 );
-
-CREATE TABLE train(
-	train_id INT NOT NULL AUTO_INCREMENT,
-    train_name VARCHAR(10),
-    PRIMARY KEY(train_id)
-);
-
-select * from train;	
-
-CREATE TABLE staff (
-	staff_id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(50),
-    fullname VARCHAR(50),
-    telephoneNumber VARCHAR(10),
-    email VARCHAR(50),
-    gender INT,
-    address_id INT,
-    PRIMARY KEY(staff_id)
+CREATE TABLE chair (
+	chair_id INT NOT NULL AUTO_INCREMENT,
+    wagon_id INT, 
+    user_id INT,
+    state VARCHAR(10),
+    PRIMARY KEY(chair_id),
+    chair_type VARCHAR(10),
+    FOREIGN KEY(wagon_id) REFERENCES wagon(wagon_id)
 );
 
 
@@ -87,10 +82,10 @@ INSERT INTO address(communce, distict, city, country) VALUES ("Vinh Loc A", "Bin
 SELECT * FROM address;
 
 INSERT INTO user(email, fullname, phoneNumber, gender, address_id) 
-VALUES ("phuckhoa81@gmail.com", "nguyen khoa minh phuc", "097295038", 0, 1), 
-		("dieuuyen8812012@gmail.com", "nguyen khoa dieu uyen", "097295038", 1, 1), 
-        ("mphuc8671@gmail.com", "nguyen khoa minh phuc", "097295038", 0, 1), 
-        ("phucnkmps24819@fpt.edu.vn", "nguyen khoa minh phuc", "097295038", 0, 1);
+VALUES ("phuckhoa81@gmail.com", "nguyen khoa minh phuc", "0972495038", 0, 1), 
+		("dieuuyen8812012@gmail.com", "nguyen khoa dieu uyen", "0972495038", 1, 1), 
+        ("mphuc8671@gmail.com", "nguyen khoa minh phuc", "0972495038", 0, 1), 
+        ("phucnkmps24819@fpt.edu.vn", "nguyen khoa minh phuc", "0972495038", 0, 1);
         
         
 INSERT INTO staff(email, fullname, telephoneNumber, gender, address_id, title) 
