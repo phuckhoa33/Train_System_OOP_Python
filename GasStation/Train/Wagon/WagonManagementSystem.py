4815ba21718b8cfad16cd27f3bdd06f02275960d
phuckhoa33 <phuckhoa81@gmail.com>
2023-05-08 18:35:26 +0700

    Fix connection of mysql in WagonManagementSystem.py

--- GasStation/Train/Wagon/WagonManagementSystem.py
+++ GasStation/Train/Wagon/WagonManagementSystem.py
@@ -27,11 +27,14 @@ class TailHeadManagementSystem(WagonManagementSystemBaseClass):
 class PassengerManagementSystem(WagonManagementSystemBaseClass):
     def __init__(self, code: int) -> None:
         super().__init__(code)
+        print(self.__type_chair)
 
     @property
     def __type_chair(self):
+        self.database.connect()
         query = f"SELECT chair_type FROM chair WHERE wagon_id = {self.code}"
         result = self.database.query_have_return(query)
+        self.database.disconnect()
         return result[0]['chair_type']
     
     def create_new_chair_or_room_or_insert_goods(self):
